b2ef972797e80c2a7ae695b8122ec1a6933be4e7
Arpan Chatterjee <arpanchatterjeeg@gmail.com>
2023-11-16 14:19:44 -0500

    uncommented cognito_login workflow

--- mobypick_app/views.py
+++ mobypick_app/views.py
@@ -1,20 +1,16 @@
 from django.shortcuts import render, redirect
-#from mobypick_app.hidden import client_id
+from mobypick_app.hidden import client_id
 
 def landing_page(request):
     return render(request, 'landing_page.html')
 
 def select_books(request):
-
     # items to be collected from databse, prepopulating with random values for now
     items = [{'image':'https://m.media-amazon.com/images/I/511x4eT16mL._SY466_.jpg', 'author': 'Shakespeare','name':'OTHELLO'},
              {'image':'https://images-worker.bonanzastatic.com/afu/images/069e/8d8f/be88_6591207841/51vvyfgws_2bl._sl1500_.jpg', 'author': 'Herman Melville','name':'MOBY DICK'}]
     return render(request, 'select_books.html', {'items': items})
 
-#def cognito_login(request):
-#    uri = "https%3A%2F%2Flocalhost%3A8000"
-#    return redirect(f"https://mobypick.auth.us-east-1.amazoncognito.com/login?client_id={client_id}&response_type=code&scope=email+openid+phone&redirect_uri={uri}")
+def cognito_login(request):
+    uri = "https%3A%2F%2Flocalhost%3A8000"
+    return redirect(f"https://mobypick.auth.us-east-1.amazoncognito.com/login?client_id={client_id}&response_type=code&scope=email+openid+phone&redirect_uri={uri}")
 
-
-def bookgame(request):
-    return render(request, 'bookgame.html')
--- mobypick_proj/urls.py
+++ mobypick_proj/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from mobypick_app.views import landing_page, select_books #, cognito_login
+from mobypick_app.views import landing_page, select_books, cognito_login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', landing_page, name='landing_page'),
-    #path('cognito_login/', cognito_login, name='cognito_login'),
+    path('cognito_login/', cognito_login, name='cognito_login'),
     path('select_books/', select_books, name='select_books'),
 ]
