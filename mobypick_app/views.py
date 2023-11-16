bc093b7f4da557ed4ed2af095b0183069fe70dbe
Anubhav Dinkar <ad6641@nyu.edu>
2023-11-16 12:18:24 -0500

    added book game page

--- mobypick_app/views.py
+++ mobypick_app/views.py
@@ -5,6 +5,7 @@ def landing_page(request):
     return render(request, 'landing_page.html')
 
 def cognito_login(request):
+    #TODO: change this callback URL to mobypick bookgame URL, after enabling HTTPS on EB
     callback_url = "https%3A%2F%2Flocalhost%3A8000"
     return redirect(f"https://mobypick.auth.us-east-1.amazoncognito.com/login?client_id={client_id}&response_type=code&scope=email+openid+phone&redirect_uri={callback_url}")
 
