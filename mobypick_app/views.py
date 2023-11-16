7ed377c57f4c9604c23a1ade4e26562004258b80
Anubhav Dinkar <ad6641@nyu.edu>
2023-11-16 12:18:16 -0500

    added game page

--- mobypick_app/views.py
+++ mobypick_app/views.py
@@ -5,8 +5,8 @@ def landing_page(request):
     return render(request, 'landing_page.html')
 
 def cognito_login(request):
-    callback_uri = "https%3A%2F%2Fmobypick.us-east-1.elasticbeanstalk.com%2Fbookgame"
-    return redirect(f"https://mobypick.auth.us-east-1.amazoncognito.com/login?client_id={client_id}&response_type=code&scope=email+openid+phone&redirect_uri={callback_uri}")
+    callback_url = "https%3A%2F%2Flocalhost%3A8000"
+    return redirect(f"https://mobypick.auth.us-east-1.amazoncognito.com/login?client_id={client_id}&response_type=code&scope=email+openid+phone&redirect_uri={callback_url}")
 
 
 def bookgame(request):
