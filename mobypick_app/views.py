72419a906cbb368e21e76debb37442f9605a654b
Arpan Chatterjee <arpanchatterjeeg@gmail.com>
2023-11-16 12:55:01 -0500

    Added basic templates for landing page and select_books

--- mobypick_app/views.py
+++ mobypick_app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
-from mobypick_app.hidden import client_id
+#from mobypick_app.hidden import client_id
 
 def landing_page(request):
     return render(request, 'landing_page.html')
 
-def cognito_login(request):
-    uri = "https%3A%2F%2Flocalhost%3A8000"
-    return redirect(f"https://mobypick.auth.us-east-1.amazoncognito.com/login?client_id={client_id}&response_type=code&scope=email+openid+phone&redirect_uri={uri}")
+def select_books(request):
+    return render(request, 'select_books.html')
+
+#def cognito_login(request):
+#    uri = "https%3A%2F%2Flocalhost%3A8000"
+#    return redirect(f"https://mobypick.auth.us-east-1.amazoncognito.com/login?client_id={client_id}&response_type=code&scope=email+openid+phone&redirect_uri={uri}")
