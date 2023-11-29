63a95288e60ac77a9ed7975a18dc8b373daa905e
Anubhav Dinkar <ad6641@nyu.edu>
2023-11-29 18:10:54 -0500

    removed navbar code

--- mobypick_app/views.py
+++ mobypick_app/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render, redirect
 from mobypick_app.hidden import client_id
 
+class Book:
+    def __init__(self, poster, author, title):
+        self.poster = poster
+        self.author = author
+        self.title = title
+
 def landing_page(request):
     return render(request, 'landing_page.html')
 
 def select_books(request):
-    # items to be collected from databse, prepopulating with random values for now
+    # TODO: from the below 10 genres, get 10 books each, prompt user to select at most 10 books that appeals to them the most among these 100
+    genre_choices = ['fiction', 'mystery_thriller_crime', 'romance', 'non_fiction', 'history_biography', 'fantasy_paranormal', 'children', 'young_adult', 'comics_graphic', 'poetry']
+
+
     items = [{'image':'https://m.media-amazon.com/images/I/511x4eT16mL._SY466_.jpg', 'author': 'Shakespeare','name':'OTHELLO'},
              {'image':'https://images-worker.bonanzastatic.com/afu/images/069e/8d8f/be88_6591207841/51vvyfgws_2bl._sl1500_.jpg', 'author': 'Herman Melville','name':'MOBY DICK'}]
     return render(request, 'select_books.html', {'items': items})
