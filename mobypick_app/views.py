41459377619d7ee891e76dab3d17eba3ff0bf807
Anubhav Dinkar <ad6641@nyu.edu>
2023-12-04 08:59:29 -0500

    added the DynamoDB scan code in a (hidden) helper; preparing for EB deploy

--- mobypick_app/views.py
+++ mobypick_app/views.py
@@ -10,6 +10,6 @@ def select_books(request):
     return render(request, 'select_books.html', {'items':items})
 
 def cognito_login(request):
-    # uri = "https%3A%2F%2Fmobypick.us-east-1.elasticbeanstalk.com%2Fselect_books"
-    uri = "https%3A%2F%2Flocalhost%3A8000%2Fselect_books"
+    uri = "https%3A%2F%2Fmobypick.us-east-1.elasticbeanstalk.com%2Fselect_books"
+    # uri = "https%3A%2F%2Flocalhost%3A8000%2Fselect_books"
     return redirect(f"https://mobypick.auth.us-east-1.amazoncognito.com/login?client_id={client_id}&response_type=code&scope=email+openid+phone&redirect_uri={uri}")    
